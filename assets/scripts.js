8995e055f0ed7bde13342211789e9ab607b5d40c
Dain <dn61345p@pace.edu>
2022-12-07 13:41:34 -0500

    lab9stuff

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,6 +15,7 @@ function addToCart(cookie) {
    itemCount = itemCount+1
    console.log(itemCount)
 
+document.getElementById("cartItems").innerHTML = itemcount
 
    //add the correct price to the currentPrice variable
 if (cookie =="peanut butter") {
@@ -26,10 +27,29 @@ currentPrice = currentPrice + 35
 } else if (cookie == "chocolate chip") {
     currentPrice = currentPrice + 25
 }
+document.querySelector(".hoverText").innerHTML = currentPrice
+
+console.log(currentPrice)
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
+    //window.alert (`You have a total of $(itemCount) items that will cost $$(currentPrice) We appreciate you shopping with us!! )
+    window.prompt (`Please prove us with your name and address! Buy 20 cookies and get the 21st cookie on us`)
+
+    currentPrice = 0
+    itemCount = 0
+
+    document.getElementById("cartItems").InnerHTML = itemCount 
+    document.querySelector(".hoverText").innerHTML = currentPrice
+
+}
+
+function darkMode() {
+    
+    document.querySelector("body").style.backgroundColor = "black"
+    document.querySelector("main").style.color = "white"
+    document.querySelector("h1").style.color = "white"
     
 }
